bbab60a64f356696521efde80a6b8a2af4e382ae
Brian Alonso <alonsobrian2@gmail.com>
2025-12-23 21:46:39 -0500

    Support latex output

--- components/ChatMessage.tsx
+++ components/ChatMessage.tsx
@@ -1,5 +1,7 @@
 import ReactMarkdown from "react-markdown";
+import rehypeKatex from "rehype-katex";
 import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
 import CopyButton from "./CopyButton";
 
 export default function ChatMessage({ m }: { m: { role: string; content: string } }) {
@@ -7,7 +9,10 @@ export default function ChatMessage({ m }: { m: { role: string; content: string 
         <div className="my-10">
             <strong>{m.role}</strong>
             <article className="max-w-none">
-                <ReactMarkdown remarkPlugins={[remarkGfm]} >
+                <ReactMarkdown
+                    remarkPlugins={[remarkGfm, remarkMath]}
+                    rehypePlugins={[rehypeKatex]}
+                >
                     {m.content}
                 </ReactMarkdown>
             </article>
--- todo.md
+++ todo.md
@@ -1,3 +1,6 @@
 # Todo
 
 - Custom instructions
+    - Always display equations in latex format
+- Copy for code blocks
+- Syntax highlighting
